508e76eb40e960e6ee98c8d0a69828694585b38f
Ilija <omniiknight@hotmail.com>
2018-10-29 18:40:08 +0100

    Initial commit -Adding home and part of portfolio

---
+++ app.js
@@ -0,0 +1,13 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', '/assets/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
+
+
+let port = document.querySelector("#portfolio-nav");
+let portCont = document.querySelector(".portfolio");
+
+port.addEventListener("click", () => {
+  portCont.classList.add("shown");
+ 
+})
