aa3de9762fcf40915e1c779840d73fa7287cc130
Ilija <omniiknight@hotmail.com>
2018-11-07 11:45:54 +0100

    particles checking cdn

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //  ------------- PARTICLES ---------------
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'assets/particles.json', function () {
-	// console.log('callback - particles.js config loaded');
+particlesJS.load('particles-js', 'particles.json', function () {
+	console.log('callback - particles.js config loaded');
 });
 
 
