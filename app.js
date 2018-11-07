47c0e16e33fc86600b2dd15f741e35bc51aef768
Ilija <omniiknight@hotmail.com>
2018-11-07 11:49:00 +0100

    particles checking cdn2 - from assets

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //  ------------- PARTICLES ---------------
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'particles.json', function () {
+particlesJS.load('particles-js', '/assets/particles.json', function () {
 	console.log('callback - particles.js config loaded');
 });
 
